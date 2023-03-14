60cb25999aa988408070f0741b036da3cd12d641
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-03-14 16:52:17 +0100

    add cd command

--- src/uInternalProcs.pp
+++ src/uInternalProcs.pp
@@ -1,6 +1,8 @@
 {$mode fpc}
 unit uInternalProcs;
 
+{$H+}
+
 { uInternalProcs.pp ; Implementations for DEASH internal commands/procedures }
 { Author: Marie Eckert                                                       }
 
@@ -10,6 +12,39 @@ interface
   function DoInternalCmd(const AName: String; const AParams: TStringDynArray): TInvokeResult;
 implementation
   uses uScriptEngine;
+
+  function Cd(const AParams: TStringDynArray): TInvokeResult;
+  var
+    dir: String;
+  begin
+    if Length(AParams) = 0 then
+    begin
+      Cd.code := -1;
+      Cd.message := 'cd expects 1 argument: <path>';
+      exit;
+    end;
+
+    if AParams[0][1] = PathDelim then
+      dir := AParams[0]
+    else
+      dir := ExpandFileName(GetCurrentDir()+PathDelim+AParams[0]);
+
+    if not DirectoryExists(dir) then
+    begin
+      Cd.code := -1;
+      Cd.message := 'No such directory: '+dir;
+      exit;
+    end;
+
+    if not SetCurrentDir(dir) then
+    begin
+      Cd.code := -1;
+      Cd.message := 'Could not change directory (an unknown error occured)';
+      exit;
+    end;
+
+    Cd.code := 0;
+  end;
 
   procedure Purr;
   const
@@ -24,9 +59,10 @@ implementation
     writeln(CAT, ' exported procedures: ');
   end;
 
-  function DoInternalCmd(const AName: String; const AParams:TStringDynArray): TInvokeResult;
+  function DoInternalCmd(const AName: String; const AParams: TStringDynArray): TInvokeResult;
   begin
     case AName of
+    'cd': DoInternalCmd := Cd(AParams);
     'purr': Purr;
     else
       DoInternalCmd.code := -1;
